84959f0684f84c92e9744d2b02feb6a97eabe2c5
Jan van den Ende <janvandenende@gmail.com>
2017-11-23 11:48:03 +0100

    add sample app with list of all currencies with their rates

--- app/src/main/java/com/getroadmap/curry/MainActivity.kt
+++ app/src/main/java/com/getroadmap/curry/MainActivity.kt
@@ -2,6 +2,7 @@ package com.getroadmap.curry
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import com.getroadmap.lib.models.ForExRates
 import com.getroadmap.lib.request.CurryApiClient
@@ -9,12 +10,20 @@ import io.reactivex.SingleObserver
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rateAdapter = RateAdapter(this, emptyList(), {
+            Log.d("DEBUG", it.toString())
+        })
+
+        recyclerview.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        recyclerview.adapter = rateAdapter
 
         CurryApiClient(this).service.getLatest()
                 .subscribeOn(Schedulers.computation())
@@ -26,6 +35,8 @@ class MainActivity : AppCompatActivity() {
 
                     override fun onSuccess(t: ForExRates) {
                         Log.d("DEBUG", t.toString())
+                        rateAdapter.updateRates(t.rates.getRates())
+                        base.setText("1 ${t.base} = ")
                     }
 
                     override fun onSubscribe(d: Disposable) {
