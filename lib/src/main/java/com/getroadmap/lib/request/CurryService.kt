2a16ebc6277ea5772d8536948ab49599ab908bf1
Jan van den Ende <janvandenende@gmail.com>
2017-11-23 12:15:30 +0100

    add more calls

--- lib/src/main/java/com/getroadmap/lib/request/CurryService.kt
+++ lib/src/main/java/com/getroadmap/lib/request/CurryService.kt
@@ -4,13 +4,36 @@ import com.getroadmap.lib.models.ForExRates
 
 import io.reactivex.Single
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 /**
  * Created by jan on 21/11/17.
+ *
+ * for documentation: see http://fixer.io/
  */
 
 interface CurryService {
+    /**
+     * Rates are quoted against the Euro by default.
+     * Quote against a different currency by setting the base parameter in your request.
+     * https://api.fixer.io/latest?base=EUR
+     */
     @GET("latest")
     fun getLatest(@Query("base") currencyCode: String = "EUR"): Single<ForExRates>
+
+    /**
+     * Get historical rates for any day since 1999.
+     * https://api.fixer.io/2000-01-03
+     */
+    @GET("{date}")
+    fun getHistorical(@Path("date") date: String): Single<ForExRates>
+
+
+    /**
+     * Request specific exchange rates by setting the symbols parameter.
+     * https://api.fixer.io/latest?symbols=USD,GBP
+     */
+    @GET("latest")
+    fun getSpecificRates(@Query("symbols") symbols: String)
 }
